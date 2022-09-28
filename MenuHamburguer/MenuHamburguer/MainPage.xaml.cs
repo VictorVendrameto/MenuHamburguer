f8b6ca6f124bb751b31cad26ef649cac82dab22d
VictorVendrameto <vendrametovictor@gmail.com>
2022-09-28 15:52:36 -0300

    Primeiro Commit

--- MenuHamburguer/MenuHamburguer/MainPage.xaml.cs
+++ MenuHamburguer/MenuHamburguer/MainPage.xaml.cs
@@ -6,13 +6,94 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace MenuHamburguer
+using AppNovaGuiaCursoDS.Views;
+
+namespace AppNovaGuiaCursoDS
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
+        }
+
+        private void Open_Inicial(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Inicial)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita", ex.Message, "OK");
+            }
+        }
+
+        private void Open_Primeiro(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Primeiro)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita", ex.Message, "OK");
+            }
+        }
+
+        private void Open_Segundo(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Segundo)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita", ex.Message, "OK");
+            }
+        }
+
+        private void Open_Terceiro(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Terceiro)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita", ex.Message, "OK");
+            }
+        }
+
+        private void Open_Vestibulinho(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Vestibulinho)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita", ex.Message, "OK");
+            }
+        }
+
+        private void Open_Contato(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Contato)));
+                IsPresented = false;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita", ex.Message, "OK");
+            }
         }
     }
 }
---
+++ MenuHamburguer/MenuHamburguer/Model/Componente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuHamburguer.Model
+{
+    public class Componente
+    {
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public string AtbResponsa { get; set; }
+        public string ValoresAtitudes { get; set; }
+        public string Competencias { get; set; }
+        public string Habilidades { get; set; }
+        public string BasesTecnologicas { get; set; }
+    }
+}
