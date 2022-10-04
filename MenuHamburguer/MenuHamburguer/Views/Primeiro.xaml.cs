849c4f89634e3c9ae5f8068a1a0ecfb82efa9c5b
VictorVendrameto <vendrametovictor@gmail.com>
2022-10-04 15:54:56 -0300

    segundo commit

--- MenuHamburguer/MenuHamburguer/Views/Primeiro.xaml.cs
+++ MenuHamburguer/MenuHamburguer/Views/Primeiro.xaml.cs
@@ -16,5 +16,90 @@ namespace MenuHamburguer.Views
         {
             InitializeComponent();
         }
+
+        //Linguagem, Trabalho e Tecnologia
+        private async void btn_LTT(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new VerComponente
+                {
+                    Nome = "Linguagem, Trabalho e Tecnologia",
+
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulário técnico da área e " +
+                                                   "Elaborar registros e planilhas de acompanhamento e controle de atividades",
+
+                    ValoresAtitudes = "Incentivar o dialogo e a interlocução. * Responsabilizar-se pela produção, utilização e " +
+                                      "divulgação de informações. * Estimular a proatividade. ",
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
+        }
+
+        //ProAlg
+        private void btn_PA(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new VerComponente
+                {
+                    Nome = "Programação e Algoritmos",
+
+                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulário técnico da área e " +
+                                                   "Elaborar registros e planilhas de acompanhamento e controle de atividades",
+
+                    ValoresAtitudes = "Incentivar o dialogo e a interlocução. * Responsabilizar-se pela produção, utilização e " +
+                                      "divulgação de informações. * Estimular a proatividade. ",
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
+        }
+
+        //Banco
+        private void btn_BD1(object sender, EventArgs e)
+        {
+
+        }
+
+        //Análise
+        private void btn_APS(object sender, EventArgs e)
+        {
+
+        }
+
+        //Design
+        private void btn_Design(object sender, EventArgs e)
+        {
+
+        }
+
+        //ProWeb
+        private void btn_PW1(object sender, EventArgs e)
+        {
+
+        }
+
+        //Fundamentos
+        private void btn_FI(object sender, EventArgs e)
+        {
+
+        }
+
+        //Tecnicas de Programação
+        private void btn_TP(object sender, EventArgs e)
+        {
+
+        }
+
+        
+              
     }
 }
