b627d616eee3568db94964ec4b264d5397abb2a8
VictorVendrameto <vendrametovictor@gmail.com>
2022-10-06 13:41:05 -0300

    terceiro commit

--- MenuHamburguer/MenuHamburguer/Views/Primeiro.xaml.cs
+++ MenuHamburguer/MenuHamburguer/Views/Primeiro.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MenuHamburguer.Model;
 
 namespace MenuHamburguer.Views
 {
@@ -22,15 +23,15 @@ namespace MenuHamburguer.Views
         {
             try
             {
-                var c = new VerComponente
+                var c = new Componente
                 {
                     Nome = "Linguagem, Trabalho e Tecnologia",
 
-                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulário técnico da área e " +
+                    AtbResponsa = "Comunicar-se em língua portuguesa, utilizando o vocabulário técnico da área e " +
                                                    "Elaborar registros e planilhas de acompanhamento e controle de atividades",
 
                     ValoresAtitudes = "Incentivar o dialogo e a interlocução. * Responsabilizar-se pela produção, utilização e " +
-                                      "divulgação de informações. * Estimular a proatividade. ",
+                                      "divulgação de informações. * Estimular a proatividade. "
                 };
                 await Navigation.PushAsync(new VerComponente(c));
             }
@@ -45,15 +46,15 @@ namespace MenuHamburguer.Views
         {
             try
             {
-                var c = new VerComponente
+                var c = new Componente
                 {
                     Nome = "Programação e Algoritmos",
 
-                    AtribuicoesResponsabilidades = "Comunicar-se em língua portuguesa, utilizando o vocabulário técnico da área e " +
+                    AtbResponsa = "Comunicar-se em língua portuguesa, utilizando o vocabulário técnico da área e " +
                                                    "Elaborar registros e planilhas de acompanhamento e controle de atividades",
 
                     ValoresAtitudes = "Incentivar o dialogo e a interlocução. * Responsabilizar-se pela produção, utilização e " +
-                                      "divulgação de informações. * Estimular a proatividade. ",
+                                      "divulgação de informações. * Estimular a proatividade. "
                 };
                 await Navigation.PushAsync(new VerComponente(c));
             }
@@ -66,37 +67,133 @@ namespace MenuHamburguer.Views
         //Banco
         private void btn_BD1(object sender, EventArgs e)
         {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Banco de Dados I",
 
+                    AtbResponsa = "Modelar Banco de Dados ",
+                                                   
+
+                    ValoresAtitudes = "Estimular a organização. * Fortalecer a persistência e o interesse na resolução " +
+                                      "de situações-problema. * Promover ações que considerem o respeito às normas estabelecidas. "
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
         }
 
         //Análise
         private void btn_APS(object sender, EventArgs e)
         {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Análise e Projeto de Sistemas",
 
+                    AtbResponsa = "Elaborar projetos de sistema de informação. ",
+
+
+                    ValoresAtitudes = "Estimular a organização. *Incentivar a criatividade. * Fortalecer a persistência " +
+                                      "e o interesse na resolução de situações-problema.  ",
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
         }
 
         //Design
         private void btn_Design(object sender, EventArgs e)
         {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Design Digital",
 
+                    AtbResponsa = "Desenvolver elementos gráficos para aplicativos e sites. ",
+
+
+                    ValoresAtitudes = "Incentivar a criatividade. * Respeitar as manifestações culturais de outros povos. " +
+                                      "* Incentivar ações que promovam a cooperação. ",
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
         }
 
         //ProWeb
         private void btn_PW1(object sender, EventArgs e)
         {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Programação Web I",
 
+                    AtbResponsa = "Desenvolver sites para Web. ",
+
+
+                    ValoresAtitudes = "Incentivar a criatividade. * Estimular a organização. * Fortalecer a persistencia " +
+                                      "e o interesse na resolução de situações-problema. ",
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
         }
 
         //Fundamentos
         private void btn_FI(object sender, EventArgs e)
         {
-
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Fundamentos da Informática",
+                    AtbResponsa = "Operar sistemas computacionais.  ",
+                    ValoresAtitudes = "Desenvolver a criticidade. * Incentivar comportamentos éticos. * Promover ações que considerem o respeito às normas estabelecidas.  "
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
         }
 
         //Tecnicas de Programação
         private void btn_TP(object sender, EventArgs e)
         {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Técnicas de Programação",
 
+                    AtbResponsa = "Desenvolver programas de computador, utilizando princípios de boas práticas. • Realizar versionamento no desenvolvimento de programas.  • Verificar usabilidade no desenvolvimento de programas.  ",
+                    
+                    ValoresAtitudes = "Incentivar a criatividade. • Incentivar comportamentos éticos. • Fortalecer a persistência e o interesse na resolução de situações-problema. "
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
         }
 
         
--- MenuHamburguer/MenuHamburguer/Views/Segundo.xaml.cs
+++ MenuHamburguer/MenuHamburguer/Views/Segundo.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using MenuHamburguer.Model;
+
 namespace MenuHamburguer.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +17,208 @@ namespace MenuHamburguer.Views
         public Segundo()
         {
             InitializeComponent();
+        }
+
+        private void btn_Ing(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Inglês Instrumental",
+                    AtbResponsa = "Comunicar-se em língua estrangeira – inglês, utilizando o vocabulário e a terminologia da área.  ",
+                    ValoresAtitudes = "Respeitar as manifestações culturais de outros povos. * Estimular a comunicação nas relações interpessoais. * Socializar os saberes.  "
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
+        }
+
+        private void btn_DS(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Desenvolvimento de Sistemas",
+                    AtbResponsa = "Analisar e projetar sistemas de informação, selecionando linguagens de programação e ambientes de desenvolvimento de acordo com as especificidades do projeto. • Codificar e depurar programas. ",
+                    ValoresAtitudes = "Fortalecer a persistência e o interesse na resolução de situações-problema. • Incentivar ações que promovam a cooperação. • Desenvolver a criatividade. "
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
+        }
+        private void btn_BD2(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Banco de Dados II ",
+                    AtbResponsa = "Implementar banco de dados. ",
+                    ValoresAtitudes = "Incentivar a criatividade. • Desenvolver a criticidade. • Fortalecer a persistência e o interesse na resolução de situações-problema "
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
+        }
+        private void btn_IP(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Internet e Protocolos ",
+                    AtbResponsa = "Utilizar protocolos de redes e internet para comunicação de dados.  ",
+                    ValoresAtitudes = "Incentivar comportamentos éticos. • Promover ações que considerem o respeito às normas estabelecidas. • Fortalecer a persistência e o interesse na resolução de situações-problema.  "
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_4(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Programação de Aplicativos Mobile I ",
+                    AtbResponsa = "Elaborar projetos de aplicativos para plataformas móveis. ",
+                    ValoresAtitudes = "Incentivar a criatividade. • Responsabilizar-se pela produção, utilização e divulgação de informações. • Estimular a organização.  "
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_5(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Programação Web II",
+                    AtbResponsa = "Planejar projetos de sistemas de informação para web ",
+                    ValoresAtitudes = "Responsabilizar-se pela produção, utilização e divulgação de informações. • Fortalecer a persistência e o interesse na resolução de situações-problema. • Incentivar a criatividade.  "
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_6(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Planejamento do Trabalho de Conclusão de Curso (TCC) em Desenvolvimento de Sistemas ",
+                    AtbResponsa = "Planejar projetos de sistemas computacionais ",
+                    ValoresAtitudes = "Comunicar-se com a equipe com clareza e objetividade. • Organizar procedimentos de maneira diversa, visando melhor eficiência "
+                };
+
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+    }
+}
+
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
+        }
+        private void btn_APM(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Programação Web I",
+
+                    AtbResponsa = "Desenvolver sites para Web. ",
+
+
+                    ValoresAtitudes = "Incentivar a criatividade. * Estimular a organização. * Fortalecer a persistencia " +
+                                      "e o interesse na resolução de situações-problema. ",
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
+        }
+        private void btn_PW2(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Programação Web I",
+
+                    AtbResponsa = "Desenvolver sites para Web. ",
+
+
+                    ValoresAtitudes = "Incentivar a criatividade. * Estimular a organização. * Fortalecer a persistencia " +
+                                      "e o interesse na resolução de situações-problema. ",
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
+        }
+        private void btn_PTCC(object sender, EventArgs e)
+        {
+            try
+            {
+                var c = new Componente
+                {
+                    Nome = "Programação Web I",
+
+                    AtbResponsa = "Desenvolver sites para Web. ",
+
+
+                    ValoresAtitudes = "Incentivar a criatividade. * Estimular a organização. * Fortalecer a persistencia " +
+                                      "e o interesse na resolução de situações-problema. ",
+                };
+                await Navigation.PushAsync(new VerComponente(c));
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Eita!", ex.Message, "OK");
+            }
         }
     }
 }
